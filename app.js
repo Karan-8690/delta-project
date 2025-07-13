1b15de3aa29263e0dd39b063578644710743fdc7
Karan Salunkhe <karan@example.com>
2025-07-13 18:55:25 +0530

    new file

--- app.js
+++ app.js
@@ -22,24 +22,14 @@ const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
 
-const dbUrl = process.env.ATLASDB_URL;
-
-main().then(() => {
-    console.log("connected to DB");
-}).catch((err) => {
-    console.log(err);
-})
-
-async function main() {
-    await mongoose.connect(dbUrl);
-}
-
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
+
+const dbUrl = process.env.ATLASDB_URL;
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
@@ -48,6 +38,7 @@ const store = MongoStore.create({
     },
     touchAfter:24*3600,
 })
+
 
 store.on("error", () => {
     console.log("ERROR IN MONGO SESSION STORE", err);
@@ -65,12 +56,6 @@ const sessionOptions = {
     }
 };
 
-// app.get("/", (req, res) => {
-//     res.send("Hi, I am root");
-// })
-
-
-
 app.use(session(sessionOptions));
 app.use(flash());
 
@@ -80,6 +65,19 @@ passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+const MONGO_URL='mongodb://127.0.0.1:27017/wanderlust';
+
+main().then(() => {
+    console.log("connected to DB");
+}).catch((err) => {
+    console.log(err);
+})
+
+async function main() {
+    await mongoose.connect(dbUrl);
+}
+
 
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
@@ -101,9 +99,9 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
-// app.all("*", (req, res, next) => {
-//     next(new ExpressError(404, "Page Not Found"));
-// })
+app.all("/", (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found"));
+})
 
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = "Something went wrong" } = err;
